5e00f22f60b08bb83bd1da5a821f1623c3358fe4
Yanjun Shao <19307110036@fudan.edu.cn>
2024-10-01 00:16:04 -0400

    update OC less memory usage

--- utils/download_model_weight_OC.py
+++ utils/download_model_weight_OC.py
@@ -2,34 +2,25 @@ import os
 import numpy as np
 import torch
 import open_clip
+
+
 open_clip.list_pretrained()
-model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-16', pretrained='laion2b_s34b_b88k')
-
-model, _, preprocess = open_clip.create_model_and_transforms('coca_ViT-B-32', pretrained='mscoco_finetuned_laion2b-s13b-b90k')
-
-model, _, preprocess = open_clip.create_model_and_transforms('ViT-L-14', pretrained='laion400m_e31')
-
-model, _, preprocess = open_clip.create_model_and_transforms('convnext_base', pretrained='laion400m_s13b_b51k')
-
-model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32', pretrained='laion400m_e31')
-
-model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32', pretrained='datacomp_m_s128m_b4k')
-
-model, _, preprocess = open_clip.create_model_and_transforms('RN50', pretrained='openai')
-
-model, _, preprocess = open_clip.create_model_and_transforms('ViT-L-14', pretrained='datacomp_xl_s13b_b90k')
-
-model, _, preprocess = open_clip.create_model_and_transforms('convnext_large_d', pretrained='laion2b_s26b_b102k_augreg')
-
-model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32', pretrained='commonpool_m_basic_s128m_b4k')
-
-
-model, _, preprocess = open_clip.create_model_and_transforms('convnext_xxlarge', pretrained='laion2b_s34b_b82k_augreg_soup')
-
-model, _, preprocess = open_clip.create_model_and_transforms('ViT-L-14', pretrained='commonpool_xl_clip_s13b_b90k')
-
-
-model, _, preprocess = open_clip.create_model_and_transforms('EVA02-E-14', pretrained='laion2b_s4b_b115k')
-
-model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32', pretrained='laion2b_s34b_b79k')
+open_clip.create_model_and_transforms('ViT-B-16', pretrained='laion2b_s34b_b88k')
+open_clip.create_model_and_transforms('coca_ViT-B-32', pretrained='mscoco_finetuned_laion2b-s13b-b90k')
+open_clip.create_model_and_transforms('ViT-L-14', pretrained='laion400m_e31')
+open_clip.create_model_and_transforms('convnext_base', pretrained='laion400m_s13b_b51k')
+open_clip.create_model_and_transforms('ViT-B-32', pretrained='laion400m_e31')
+open_clip.create_model_and_transforms('ViT-B-32', pretrained='datacomp_m_s128m_b4k')
+open_clip.create_model_and_transforms('RN50', pretrained='openai')
+open_clip.create_model_and_transforms('ViT-L-14', pretrained='datacomp_xl_s13b_b90k')
+open_clip.create_model_and_transforms('convnext_large_d', pretrained='laion2b_s26b_b102k_augreg')
+open_clip.create_model_and_transforms('ViT-B-32', pretrained='datacomp_m_s128m_b4k')
+open_clip.create_model_and_transforms('RN50', pretrained='openai')
+open_clip.create_model_and_transforms('ViT-L-14', pretrained='datacomp_xl_s13b_b90k')
+open_clip.create_model_and_transforms('convnext_large_d', pretrained='laion2b_s26b_b102k_augreg')
+open_clip.create_model_and_transforms('ViT-B-32', pretrained='commonpool_m_basic_s128m_b4k')
+open_clip.create_model_and_transforms('convnext_xxlarge', pretrained='laion2b_s34b_b82k_augreg_soup')
+open_clip.create_model_and_transforms('ViT-L-14', pretrained='commonpool_xl_clip_s13b_b90k')
+open_clip.create_model_and_transforms('EVA02-E-14', pretrained='laion2b_s4b_b115k')
+open_clip.create_model_and_transforms('ViT-B-32', pretrained='laion2b_s34b_b79k')
 
